dcac51d3c792078023140d62dc652514b8379526
Shcherbakan Dmitriy <ds@mobioinc.com>
2016-04-06 19:47:27 +0300

    class MyTargetApi refactoring

--- src/MyTargetApi.php
+++ src/MyTargetApi.php
@@ -4,25 +4,24 @@ namespace Mobio\Target;
 
 class MyTargetApi extends \Mobio\Target\Api
 {
-
 	/**
-	 * url to get the clients list
+	 * Url to get the clients list
 	 */
-	const CLIENTS_URL = "/api/v1/clients.json";
+	const CLIENTS_URL = '/api/v1/clients.json';
 
 	/**
-	 * url to get the campaigns list
+	 * Url to get the campaigns list
 	 */
-	const CAMPAIGNS_URL = "/users/%s/api/v1/campaigns.json?fields=id";
+	const CAMPAIGNS_URL = '/users/%s/api/v1/campaigns.json?fields=id';
 
 	/**
-	 * url to get the campaigns statitics list
+	 * Url to get the campaigns statitics list
 	 */
-	const CAMPAIGNS_STATS_URL = "/users/%s/api/v1/statistics/campaigns/%s/day/%s-%s.json";
+	const CAMPAIGNS_STATS_URL = '/users/%s/api/v1/statistics/campaigns/%s/day/%s-%s.json';
 
 
 	/**
-	 * list of the clients 
+	 * List of the clients 
 	 * @return array
 	 */
 	public function getClients()
@@ -31,25 +30,26 @@ class MyTargetApi extends \Mobio\Target\Api
 	}
 
 	/**
-	 * list of the user`s campaigns
-	 * @param  string $user user email
+	 * List of the user`s campaigns
+	 * @param  string $username
 	 * @return array
 	 */
-	public function getUserCampaignsList($user)
+	public function getUserCampaignsList($username)
 	{
-		return $this->request(sprintf(self::CAMPAIGNS_URL, $user))->parse();
+		return $this->request(sprintf(self::CAMPAIGNS_URL, $username))->parse();
 	}
 
 	/**
-	 * list of the user`s campaigns statistics for the period
-	 * @param  string $user      user email
+	 * List of the user`s campaigns statistics for the period
+	 * @param  string $username
 	 * @param  string $dateStart start of the period
 	 * @param  string $dateEnd   end of the period
 	 * @return array
 	 */
-	public function getUserCampaigns($user, $dateStart, $dateEnd)
+	public function getUserCampaigns($username, $dateStart, $dateEnd)
 	{
-		$campaigns = $this->getUserCampaignsList($user);
+		$campaigns = $this->getUserCampaignsList($username);
+
 		if (!empty($campaigns)) {
 			$campaigns = array_map(function($item) {
 				return $item->id;
@@ -59,7 +59,9 @@ class MyTargetApi extends \Mobio\Target\Api
 			$dateStart		= date('d.m.Y', strtotime($dateStart));
 			$dateEnd		= date('d.m.Y', strtotime($dateEnd));
 
-			return $this->request(sprintf(self::CAMPAIGNS_STATS_URL, $user, $campaignsStr, $dateStart, $dateEnd))->parse();
+			return $this->request(
+				sprintf(self::CAMPAIGNS_STATS_URL, $username, $campaignsStr, $dateStart, $dateEnd)
+			)->parse();
 		} else {
 			return [];
 		}
